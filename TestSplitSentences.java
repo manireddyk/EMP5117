d5add399635da68be38e311b93d4d01bded28dab
manireddyk <46611593+manireddyk@users.noreply.github.com>
2019-03-25 20:01:53 -0400

    Create TestSplitSentences.java

---
+++ TestSplitSentences.java
@@ -0,0 +1,16 @@
+/*
+ * Problem 1.5
+ * This program gives the standard output by splitting and printing them in the separate line from given statement.
+ */
+//introduced TestSplitSentences class
+public class TestSplitSentences{
+public  static	void main(String [ ]args) {
+System.out.println("\nTesting  \" splitSentences \"\n" ) ;
+String test ="One. Two. Another one .. This, also. With 1,#. just.more." ;
+System.out.println("The	string  \"" +test+ "\"  is  plit  into:" ) ;
+String [ ] set = StringUtils.splitSentences(test) ;
+for(int i =  0 ; i < set.length; i++ ) {
+System.out.println( "\"" + set [ i ]  + "\"" ) ;
+}
+}
+}
