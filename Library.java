6fd5c43dc142ee9b91c4b3340105dbf97d34453d
manireddyk <46611593+manireddyk@users.noreply.github.com>
2019-03-25 20:13:15 -0400

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,54 @@
+
+public class Library {
+
+	Book[] books = null;
+	int bookPointer = 0;
+	public Library(int size){
+
+		books = new Book[size];
+	}
+
+	void addBook(Book book){
+		if(bookPointer == (books.length -1)){
+			System.out.println("The library is full");
+		}
+		else{
+			books[bookPointer] = book;
+			bookPointer++;
+		}
+	}
+
+	void printLibrary(){
+		for (int i = 0; i < books.length; i++) {
+			if(books[i] != null){
+				System.out.println(books[i].toString());
+			}
+		}
+	}
+
+	void searchAuthor(String author){
+		for (int i = 0; i < books.length; i++) {
+			if(books[i] != null && books[i].author.equals(author)){
+				System.out.println(books[i].toString());
+			}
+		}
+	}
+
+	void searchTitle(String title){
+		for (int i = 0; i < books.length; i++) {
+			if(books[i] != null && books[i].title.equals(title)){
+				System.out.println(books[i].toString());
+			}
+		}
+	}
+
+	void searchYear(int year){
+		for (int i = 0; i < books.length; i++) {
+			if(books[i] != null && books[i].year == year){
+				System.out.println(books[i].toString());
+			}
+		}
+	}
+
+
+}
