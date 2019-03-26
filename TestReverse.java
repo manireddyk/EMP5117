1fb64086cccf90c29c195c4703ca7d15191f217e
manireddyk <46611593+manireddyk@users.noreply.github.com>
2019-03-25 20:00:25 -0400

    Create TestReverse.java

---
+++ TestReverse.java
@@ -0,0 +1,17 @@
+/*
+ * Problem 1.3
+ * This program is to reprint the given statement by reversing all the letters including special characters.
+ */
+
+//introduced TestReverse class
+public class TestReverse{
+public static void main (String [ ]args){
+System.out.println("\nTesting \"reverse\"\n" ) ;
+String	test = "This is some sentence." ;
+System.out.println("The	string \""+ test+ "\"  becomes  \"" +StringUtils .reverse(test )+"\"" ) ;
+test ="Words. Separated. By. Dots." ;
+System.out.println( "Thestring  \"" + test	+ "\"  becomes  \"" +StringUtils.reverse(test)	+"\"" ) ;
+test ="With trailing spaces	" ;
+System.out.println("The	string  \"" +test+ "\"  becomes  \"" +	StringUtils.reverse(test)+"\"" ) ;
+}
+}
